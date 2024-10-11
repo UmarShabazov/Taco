aa7c5379065f635d73726c874b90c95f633c9db9
Umar Shabazov <malik95i@yandex.ru>
2024-10-11 10:46:40 +0400

    Core updates related to change from JDBC to Spring JDBC

--- src/main/java/com/github/umarshabazov/tacocloud/tacos/data/IngredientRepository.java
+++ src/main/java/com/github/umarshabazov/tacocloud/tacos/data/IngredientRepository.java
@@ -1,14 +1,11 @@
 package com.github.umarshabazov.tacocloud.tacos.data;
 
 import com.github.umarshabazov.tacocloud.tacos.Ingredient;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.Repository;
 
 import java.util.Optional;
 
-public interface IngredientRepository {
+public interface IngredientRepository extends CrudRepository <Ingredient,String> {
 
-    Iterable<Ingredient> findAll();
-
-    Optional<Ingredient> findById(String id);
-
-    Ingredient save(Ingredient ingredient);
 }
--- src/main/java/com/github/umarshabazov/tacocloud/tacos/data/OrderRepository.java
+++ src/main/java/com/github/umarshabazov/tacocloud/tacos/data/OrderRepository.java
@@ -2,7 +2,8 @@ package com.github.umarshabazov.tacocloud.tacos.data;
 
 import java.util.Optional;
 import com.github.umarshabazov.tacocloud.tacos.TacoOrder;
+import org.springframework.data.repository.CrudRepository;
 
-public interface OrderRepository {
-    TacoOrder save(TacoOrder order);
+public interface OrderRepository extends CrudRepository <TacoOrder, Long> {
+
 }
